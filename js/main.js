c30542fd32a7869d9b37d55c9f53e474a71510b8
Baraasher <Baraa.programmer@gmail.com>
2023-01-14 23:08:14 +0200

    commit 3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
 let topbtn = document.getElementById("arrow");
 let card = document.getElementsByTagName("card");
 let alertTop = document.getElementById("alertTop");
+let modeBtn = document.getElementById('mode');
+let logoimg = document.getElementById('logoimg');
+
+modeBtn.onchange = (e) => {
+  if (modeBtn.checked === true) {
+ 
+document.querySelector('body').classList.toggle("lightmode");
+
+  } else {
+    document.querySelector('body').className="dark";
+
+
+  }
+}
+
+const mode = window.localStorage.getItem('mode');
+if (mode == 'dark') {
+  modeBtn.checked = true;
+  document.querySelector('body').classList.toggle("lightmode");
+
+
+}
+
+if (mode == 'light') {
+  modeBtn.checked = false;
+  document.querySelector('body').className="dark";
+
+
+
+}
+
+
+
 
 window.onscroll = function () {
   scrollFunction();
