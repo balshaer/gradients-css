a37bfa7baf47dbe184710ceb6624ad5665983250
Baraasher <Baraa.programmer@gmail.com>
2023-03-15 17:25:28 +0200

    update layout

--- js/main.js
+++ js/main.js
@@ -1,52 +1,27 @@
-//the elements
 let topbtn = document.getElementById("arrow");
 let card = document.getElementsByTagName("card");
 let alertTop = document.getElementById("alertTop");
 let modeBtn = document.getElementById('mode');
 let logoimg = document.getElementById('logoimg');
-
-
-
-//mode function
-
-
 modeBtn.onchange = (e) => {
   if (modeBtn.checked === true) {
- 
 document.querySelector('body').classList.toggle("lightmode");
-
   } else {
     document.querySelector('body').className="dark";
-
-
   }
 }
-
-
 const mode = window.localStorage.getItem('mode');
 if (mode == 'dark') {
   modeBtn.checked = true;
   document.querySelector('body').classList.toggle("lightmode");
-
-
 }
-
 if (mode == 'light') {
   modeBtn.checked = false;
   document.querySelector('body').className="dark";
-
-
-
 }
-
-
-
-//scroll function
-
 window.onscroll = function () {
   scrollFunction();
 };
-
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     topbtn.style.display = "block";
@@ -54,14 +29,10 @@ function scrollFunction() {
     topbtn.style.display = "none";
   }
 }
-
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
-//alert top function
-
 function alert() {
   document.getElementById("alertTop").style.top = "-60px";
 }
