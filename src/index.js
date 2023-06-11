d3aa7dc36ef2db37c11e71c1df8a312353c2a294
Baraa Alsher <alsher.info@gmail.com>
2023-06-11 22:52:57 +0300

    Add analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { inject } from '@vercel/analytics';
+inject();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
