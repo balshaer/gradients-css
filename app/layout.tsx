bebef0bade85776f7a4833a7fed23f3edc578eaf
Baraa Alshaer <Baraa.Programmer@gmail.com>
2024-02-11 13:35:20 +0200

    feat: Add Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=chillax@600,500,300,700,400&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+
       <body className={inter.className}>{children}</body>
     </html>
   );
