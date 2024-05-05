3ba3526dd6b3bdf571a4a5212d250407a31b77a0
Baraa Alshaer <Baraa.Programmer@gmail.com>
2024-05-05 12:27:14 +0300

    build(deps): update Gradients.tsx, Header.tsx, Navbar.tsx and package-lock.json

--- components/layouts/header/Header.tsx
+++ components/layouts/header/Header.tsx
@@ -4,8 +4,8 @@ import { useState } from "react";
 function Header() {
   return (
     <div className="w-full flex justify-center items-center flex-col  text-center gap-4 pt-20 ">
-      <h1 className=" font-bold text-3xl  text-[var(--color-headline)]">
-        Elevate Your Designs with Ease
+      <h1 className=" font-bold text-3xl bg-gradient-to-r from-[#bf5ae0] via-[#9400D3] to-[#a811da] inline-block text-transparent bg-clip-text max-md:text-2xl">
+        Gradients CSS
       </h1>
 
       <p className="text-[var(--color-paragraph)] text-base max-w-xl w-full">
--- components/layouts/navbar/Navbar.tsx
+++ components/layouts/navbar/Navbar.tsx
@@ -5,13 +5,8 @@ import React from "react";
 export default function Navbar() {
   return (
     <header>
-      <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-        <Link
-          href={"/"}
-          className=" font-bold text-xl bg-gradient-to-r from-[#bf5ae0] via-[#9400D3] to-[#a811da] inline-block text-transparent bg-clip-text"
-        >
-          Gradients CSS
-        </Link>
+      <div className="mx-auto flex h-16  items-center gap-8  ">
+   
         <div className="flex flex-1 items-center justify-end md:justify-between">
           <nav aria-label="Global" className="hidden md:block"></nav>
           <div className="flex items-center gap-4">
